b9ce8ed206ca58984aae52a480dddf4c8576a8f2
ukieTux <ukie.tux@gmail.com>
2020-08-27 23:11:39 +0800

    add appBar, navigateToClearStack on context extensions

--- lib/utils/ext_context.dart
+++ lib/utils/ext_context.dart
@@ -1,4 +1,9 @@
+import 'package:brupedia/blocs/navdrawer/navdrawer_bloc.dart';
+import 'package:brupedia/pages/pages.dart';
+import 'package:brupedia/resources/resources.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 extension ContextExtensions on BuildContext {
   bool isMobile() {
@@ -37,5 +42,45 @@ extension ContextExtensions on BuildContext {
         MaterialPageRoute(
           builder: (context) => widget,
         ));
+  }
+
+  goToClearStack(Widget widget) {
+    Navigator.pushAndRemoveUntil(
+        this, MaterialPageRoute(
+        builder: (context) =>
+            BlocProvider(
+                create: (context) => NavDrawerBloc(), child: MainPage())), (
+        route) => false);
+  }
+
+  appBar() {
+    return AppBar(
+      brightness: Brightness.light,
+      backgroundColor: Colors.white,
+      elevation: 0,
+      centerTitle: true,
+      leading: IconButton(
+        icon: Icon(Icons.arrow_back_ios, color: Palette.colorPrimary,),
+        onPressed: () {
+          Navigator.pop(this);
+        },),
+      automaticallyImplyLeading: true,
+      title: Hero(
+        tag: "logo",
+        child: Image.asset(
+          "images/ic_brupedia_color.png",
+          height: dp30(this),
+        ),
+      ),
+      actions: [
+        IconButton(
+          icon: SvgPicture.asset("images/ic_home.svg", color: Palette
+              .colorPrimary,),
+          onPressed: () {
+            goToClearStack(HomePage());
+          },
+        )
+      ],
+    );
   }
 }
